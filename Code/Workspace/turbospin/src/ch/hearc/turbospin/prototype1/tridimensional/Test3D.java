69644da550be21f3448010c9c4c4b1b6cd8ee3b6
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-03-30 15:05:58 +0200

    Add Java3D library to the project, and buildpath updated

---
+++ Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/Test3D.java
@@ -0,0 +1,9 @@
+package ch.hearc.turbospin.prototype1.tridimensional;
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+public class Test3D {
+	
+	SimpleUniverse su = new SimpleUniverse();
+
+}
---
+++ Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/UseTest3D.java
@@ -0,0 +1,9 @@
+package ch.hearc.turbospin.prototype1.tridimensional;
+
+public class UseTest3D {
+	
+	public static void main(String[] args) {
+		new Test3D();
+	}
+
+}
