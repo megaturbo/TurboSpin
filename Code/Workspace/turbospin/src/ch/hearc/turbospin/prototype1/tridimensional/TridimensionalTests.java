f89aa4bb18ab2145f2e68ec829d487e4d25947da
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-04-13 15:00:38 +0200

    3d tests

--- Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/TridimensionalTests.java
+++ Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/TridimensionalTests.java
@@ -25,7 +25,7 @@ public class TridimensionalTests extends JPanel {
         Canvas3D canvas3D = new Canvas3D(config);
         add("Center", canvas3D);
 
-        BranchGroup scene = createSceneGraph();
+        BranchGroup scene = createSceneGraph();	
 
         // SimpleUniverse is a Convenience Utility class
         SimpleUniverse u = new SimpleUniverse(canvas3D);
@@ -34,47 +34,40 @@ public class TridimensionalTests extends JPanel {
 	}
 	
 	public BranchGroup createSceneGraph() {
-		BranchGroup objRoot = new BranchGroup();
-		
-		Background background = new Background(new Color3f(1f,0,0));
-		BoundingSphere sphere = new BoundingSphere(new Point3d(0,0,0), 100000);
-		background.setApplicationBounds(sphere);
-		objRoot.addChild(background);
-
-		// Create X axis
-		LineArray axisXLines = new LineArray(2, LineArray.COORDINATES);
-		objRoot.addChild(new Shape3D(axisXLines));
-
-		axisXLines.setCoordinate(0, new Point3f(-1.0f, 0.0f, 0.0f));
-		axisXLines.setCoordinate(1, new Point3f(1.0f, 0.0f, 0.0f));
-
-		// Create Y axis
-		LineArray axisYLines = new LineArray(2, LineArray.COORDINATES
-				| LineArray.COLOR_3);
-		objRoot.addChild(new Shape3D(axisYLines));
-
-		axisYLines.setCoordinate(0, new Point3f(0.0f, -1.0f, 0.0f));
-		axisYLines.setCoordinate(1, new Point3f(0.0f, 1.0f, 0.0f));
-
-		// Create Z axis with arrow
-		Point3f z1 = new Point3f(0.0f, 0.0f, -1.0f);
-		Point3f z2 = new Point3f(0.0f, 0.0f, 1.0f);
-
-		LineArray axisZLines = new LineArray(10, LineArray.COORDINATES
-				| LineArray.COLOR_3);
-		objRoot.addChild(new Shape3D(axisZLines));
-
-		axisZLines.setCoordinate(0, z1);
-		axisZLines.setCoordinate(1, z2);
-		axisZLines.setCoordinate(2, z2);
-		axisZLines.setCoordinate(3, new Point3f(0.1f, 0.1f, 0.9f));
-		axisZLines.setCoordinate(4, z2);
-		axisZLines.setCoordinate(5, new Point3f(-0.1f, 0.1f, 0.9f));
-		axisZLines.setCoordinate(6, z2);
-		axisZLines.setCoordinate(7, new Point3f(0.1f, -0.1f, 0.9f));
-		axisZLines.setCoordinate(8, z2);
-		axisZLines.setCoordinate(9, new Point3f(-0.1f, -0.1f, 0.9f));
-
-		return objRoot;
-	}
+	    BranchGroup objRoot = new BranchGroup();
+	    
+	    // Create X axis
+	    LineArray axisXLines=new LineArray(2,LineArray.COORDINATES);
+	    objRoot.addChild(new Shape3D(axisXLines));
+	      
+	    axisXLines.setCoordinate(0,new Point3f(-1.0f,0.0f,0.0f));
+	    axisXLines.setCoordinate(1,new Point3f(1.0f,0.0f,0.0f));        
+	    
+	    // Create Y axis  
+	    LineArray axisYLines=new LineArray(2,LineArray.COORDINATES|LineArray.COLOR_3);
+	    objRoot.addChild(new Shape3D(axisYLines));
+	      
+	    axisYLines.setCoordinate(0,new Point3f(0.0f,-1.0f,0.0f));
+	    axisYLines.setCoordinate(1,new Point3f(0.0f,1.0f,0.0f));
+	      
+	    // Create Z axis with arrow
+	    Point3f z1=new Point3f(0.0f,0.0f,-1.0f);
+	    Point3f z2=new Point3f(0.0f,0.0f,1.0f);
+	      
+	    LineArray axisZLines=new LineArray(10,LineArray.COORDINATES|LineArray.COLOR_3);
+	    objRoot.addChild(new Shape3D(axisZLines));
+	      
+	    axisZLines.setCoordinate(0,z1);
+	    axisZLines.setCoordinate(1,z2);
+	    axisZLines.setCoordinate(2,z2);
+	    axisZLines.setCoordinate(3,new Point3f(0.1f,0.1f,0.9f));
+	    axisZLines.setCoordinate(4,z2);
+	    axisZLines.setCoordinate(5,new Point3f(-0.1f,0.1f,0.9f));
+	    axisZLines.setCoordinate(6,z2);
+	    axisZLines.setCoordinate(7,new Point3f(0.1f,-0.1f,0.9f));
+	    axisZLines.setCoordinate(8,z2);
+	    axisZLines.setCoordinate(9,new Point3f(-0.1f,-0.1f,0.9f));
+	      
+	    return objRoot;
+	  }
 }
