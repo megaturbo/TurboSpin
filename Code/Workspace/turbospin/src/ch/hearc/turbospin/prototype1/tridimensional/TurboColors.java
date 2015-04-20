c9b34d001a77856a1246f921795ddeffcf643913
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-04-20 17:19:15 +0200

    First view of 3d axis, with mouse tranform

---
+++ Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/TurboColors.java
@@ -0,0 +1,11 @@
+package ch.hearc.turbospin.prototype1.tridimensional;
+
+import javax.vecmath.Color3f;
+
+public class TurboColors {
+	public static final Color3f WHITE = new Color3f(1.0f, 1.0f, 1.0f);
+	public static final Color3f RED = new Color3f(1.0f, 0.0f, 0.0f);
+	public static final Color3f GREEN = new Color3f(0.0f, 1.0f, 0.0f);
+	public static final Color3f BLUE = new Color3f(0.0f, 0.0f, 1.0f);
+	public static final Color3f PINK = new Color3f(1.0f, 0.0f, 1.0f);
+}
