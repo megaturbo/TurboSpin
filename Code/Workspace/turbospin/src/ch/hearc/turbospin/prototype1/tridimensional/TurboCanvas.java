914d6591461fb367c8ab74a4f0c0a3c17e5403b5
Thomas Roulin <thomas.roulin@he-arc.ch>
2015-04-20 15:45:57 +0200

    3d merge

--- Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/TurboCanvas.java
+++ Code/Workspace/turbospin/src/ch/hearc/turbospin/prototype1/tridimensional/TurboCanvas.java
@@ -1,5 +1,6 @@
 package ch.hearc.turbospin.prototype1.tridimensional;
 
+import java.awt.Color;
 import java.awt.GraphicsConfiguration;
 import java.util.Vector;
 
@@ -8,9 +9,11 @@ import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
 import javax.media.j3d.Group;
 import javax.media.j3d.LineArray;
+import javax.media.j3d.Material;
 import javax.media.j3d.Shape3D;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
 
 import ch.hearc.turbospin.prototype1.mathtools.Line3D;
@@ -21,7 +24,6 @@ import com.sun.j3d.utils.universe.SimpleUniverse;
 public class TurboCanvas extends Canvas3D {
 	
 	BranchGroup contents = new BranchGroup();
-	 Appearance app = new Appearance();
 
 	public TurboCanvas(GraphicsConfiguration arg0) {
 		super(arg0);
@@ -44,13 +46,18 @@ public class TurboCanvas extends Canvas3D {
 		//TODO Add the Line3D to the branch group
 		// Nothing is working of the code below
 		Vector<Double> axisDirection = axis.getVectorDirection();
+
+		Appearance appearance = new Appearance();
+		
 		
 		Point3f[] plaPts = new Point3f[2];
 	    plaPts[0] = new Point3f(-0.9f, -0.7f, 0.0f);
 	    plaPts[1] = new Point3f(-0.5f, 0.7f, 0.0f);
 	    LineArray pla = new LineArray(2, LineArray.COORDINATES);
+	    pla.setColor(0, new Color3f(0.2f,0f,0f));
 	    pla.setCoordinates(0, plaPts);
-	    Shape3D plShape = new Shape3D(pla, app);
+	    Shape3D plShape = new Shape3D(pla, appearance);
+	    
 	    lineGroup.addChild(plShape);
 	    
 
